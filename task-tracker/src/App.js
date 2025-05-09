4ed818b789039247200d2baaa7bb67306b5a62d9
TetiankaSh <shh.tetiana@gmail.com>
2025-05-09 19:07:00 +0300

    first input and button with borders

---
+++ task-tracker/src/App.js
@@ -0,0 +1,52 @@
+// import logo from './logo.svg';
+import './App.css';
+import React, { use, useState } from 'react';
+
+function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if(task) {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  return (
+      <div className="App">
+       <h1>Buleika's Task Tracker</h1>
+          <div className='input-row'>
+            <div className="pencil-wrapper-square input-wrapper">
+              <input
+                type="text"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+                placeholder="Enter repo URL"
+              />
+            </div>
+
+            <div className="pencil-wrapper-square button-wrapper">
+              <button onClick={addTask}>Load issues</button>
+            </div>
+
+            <ul>
+              {tasks.map((task, index) => (
+                <li key={index}>
+                  {task}
+                  <button onClick={() => deleteTask(index)}>Delete</button>
+                </li>
+              ))}
+            </ul>
+          </div>
+      </div>
+
+  )
+}
+
+export default App;
